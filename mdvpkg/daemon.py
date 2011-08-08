8710933a310f11406f6c9af5e8417763cb9952d0
jvdm <jvdm@mandriva.com>
2011-08-08 17:09:46 -0300

    Update daemon to handle rejected-dependecy reject

--- mdvpkg/daemon.py
+++ mdvpkg/daemon.py
@@ -305,7 +305,8 @@ class DBusPackageList(PackageList, dbus.service.Object):
         list.append(rej[0])
         list.append(rpm2nvra(rej[1]))
         # Convert subjects that are RpmPackage objects ...
-        if rej[0] in {'reject-install-conflicts'}:
+        if rej[0] in {'reject-install-conflicts',
+                      'reject-install-rejected-dependency'}:
             list.append(map(lambda rpm: rpm.nvra, rej[2]))
         else:
             list.append(rej[2])
